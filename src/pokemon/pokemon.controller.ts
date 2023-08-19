a6df1de3bb35f69d6bb3f137ea0f08c9cdbf9e8b
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-08-19 13:45:24 -0300

    Fixing vercel issues

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -11,7 +11,7 @@ import {
 import { PokemonService } from './pokemon.service';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { ParseMongoIdPipe } from 'src/common/pipes/parse-mongo-id/parse-mongo-id.pipe';
+import { ParseMongoIdPipe } from '../common/pipes/parse-mongo-id/parse-mongo-id.pipe';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Controller('pokemon')
