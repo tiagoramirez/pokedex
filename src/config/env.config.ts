07717d5d24966547a6b71a3b04f72de3cadeb9cf
Tiago Ramirez <tiagoramirez2001@gmail.com>
2023-10-02 17:00:09 -0300

    Changed mongo version (with lower space) and changing env variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,7 +1,8 @@
 export const EnvConfiguration = () => ({
+  port: process.env.PORT || 3000,
   environment: process.env.NODE_ENV || 'prod',
   mongodb: process.env.MONGODB_URL,
-  port: process.env.PORT || 3000,
-  defaultLimit: process.env.DEFAULT_LIMIT || 10,
-  maxPokemonSeed: process.env.MAX_POKEMON_SEED || 100,
+  getAllLimit: process.env.GET_ALL_POKEMONS_LIMIT || 10,
+  seedLimit: process.env.SEED_LIMIT || 100,
+  logEnabled: process.env.LOG_ENABLED || 0,
 });
